555cafa96861b909198743d7d89a23f6f621b56a
HMUsama <muhammadusama995@yahoo.com>
2023-07-05 11:33:12 +0500

    git ignore file add

--- api/controllers/parents/ParentController.js
+++ api/controllers/parents/ParentController.js
@@ -5,11 +5,11 @@ module.exports = {
     addParentDetails: async function (req, res) {
         try {
             const { playerName, age, gender, } = req.body;
-            sails.log('addParentDetails---->', req)
+            // sails.log('addParentDetails---->', req.body)
 
-            // const addDetails = await Parent.create({ playerName, age, gender, parentalProof }).fetch();
+            const addDetails = await Parent.create({ playerName, age, gender, }).fetch();
 
-            // return res.json({ message: 'Add Parent Details successfully.', Parent: addDetails });
+            return res.json({ message: 'Add Parent Details successfully.', Parent: addDetails });
 
         } catch (error) {
             return res.serverError(error);
